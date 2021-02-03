3520dcd1ae16face6598a7c5eeb2fcfeb8f09c1a
himanshunete <himanshuneteh@gmail.com>
2021-02-03 08:18:52 +0530

    [himanshunete]Add MessageIsPrinted

---
+++ EmpWageComputationProgram/EmpWageBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmpWageComputationProgram
+{
+    class EmpWageBuilder
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+             
+        }
+    }
+}
