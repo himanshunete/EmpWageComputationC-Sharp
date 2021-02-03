e83c0bcd2e35d885f87dd4a5268d9b4b35b99910
himanshunete <himanshuneteh@gmail.com>
2021-02-03 13:27:46 +0530

    [himanshunete]Add FirstCommit

--- EmpWageComputationProgram/EmpWageBuilder.cs
+++ EmpWageComputationProgram/EmpWageBuilder.cs
@@ -2,13 +2,30 @@
 
 namespace EmpWageComputationProgram
 {
-    
+    class EmpWage
+    {
+        public static void CheckingAttendance()
+        {
+            //Constant
+            int IS_PRESENT = 1;
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
     class EmpWageBuilder
     {
-        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program");
+            EmpWage.CheckingAttendance();
 
         }
     }
